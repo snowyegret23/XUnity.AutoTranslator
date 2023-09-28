a2a27c8d873967f705205cde8ef2345e8f9a32a9
Snowyegret <79635537+snowyegret23@users.noreply.github.com>
2023-09-28 19:34:40 +0900

    Set DefaultTranslationDelay to 0.1f

--- src/XUnity.AutoTranslator.Plugin.Core/Configuration/Settings.cs
+++ src/XUnity.AutoTranslator.Plugin.Core/Configuration/Settings.cs
@@ -43,7 +43,7 @@ namespace XUnity.AutoTranslator.Plugin.Core.Configuration
       public static float Timeout = 150.0f;
       public static string RedirectedResourcesPath;
       public static readonly int MaxImguiKeyCacheCount = 10000;
-      public static readonly float DefaultTranslationDelay = 0.9f;
+      public static readonly float DefaultTranslationDelay = 0.1f;
       public static readonly int DefaultMaxRetries = 67;
 
       public static Dictionary<string, string> Replacements = new Dictionary<string, string>();
